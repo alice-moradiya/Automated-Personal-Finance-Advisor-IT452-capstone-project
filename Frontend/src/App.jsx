43aab047cf0121af119352b1730fe0ef519a7e4f
Alice <alice.al.moradiya@gmail.com>
2024-12-01 01:21:47 -0700

    Updating UI

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Dashboard from './Pages/Dashboard'
-import Goals from './Pages/Goals'
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Dashboard from "./Pages/Dashboard";
+import Goals from "./Pages/Goals";
 
 function App() {
   return (
-   <>
-   <Navbar/>
-   <Dashboard/>  
-   <Goals/>
-   </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        {/* Define the routes for your pages */}
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/goals" element={<Goals />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
